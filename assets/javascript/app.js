6cde0b9090cbb4f6010c51bdceb35f7a13b7bc2a
Joseph V <joseph.vonedwins@yahoo.com>
2018-06-17 12:19:13 -0400

    initial index,html wire frame with css, and first 2 question objects in js

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+
+//Variable Section
+//====================================
+
+//Question Objects====================
+
+var question1 = {
+    questionNumber: 1,
+    questionText: "What year did Neil Armstrong first set foot on the Moon?",
+    answer1: ["1969", "correct"],
+    answer2: ["1983", "wrong"], 
+    answer3: ["1965", "wrong"],
+    answer4: ["1972", "wrong"]
+}
+
+var question2 = {
+    questionNumber: 2,
+    questionText: "On April 12, 1981, the first Space Shuttle lefted off from Kennedy Space Center and entered orbit around the Earth. What was the name of this shuttle?",
+    answer1: ["Columbia", "correct"],
+    answer2: ["Challenger", "wrong"], 
+    answer3: ["Discovery", "wrong"],
+    answer4: ["Endeavour", "wrong"]
+}
+
+
+function startGame() {
+    $('#start-button')
+}
+
